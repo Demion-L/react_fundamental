d5698b7b3aa9d8208df8a39330ea333d10f48c11
dlatys <dlatys@softserveinc.com>
2022-04-22 13:42:36 +0300

    Refresh

--- src/App.js
+++ src/App.js
@@ -5,30 +5,51 @@ import StateClass from './Components/StateClass';
 import Post from './Components/Post';
 import { useState } from 'react';
 import PostForm from './Components/PostForm';
+import X_component from './Components/X_component';
+import XCompForm from './Components/X_comp_form';
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'Javascript', body: 'programming language'},
-    {id: 2, title: 'Java', body: 'programming language'},
-    {id: 3, title: 'Python', body: 'programming language'},
-    {id: 4, title: 'Golang', body: 'programming language'},
-  ])
+  // const [ posts, setPosts ] = useState( [
+  //   { id: 1, title: 'Javascript', body: 'programming language' },
+  //   { id: 2, title: 'Java', body: 'programming language' },
+  //   { id: 3, title: 'Python', body: 'programming language' },
+  //   { id: 4, title: 'Golang', body: 'programming language' },
+  // ] );
+  // const createPost = ( newPost ) => {
+  //   setPosts( [ ...posts, newPost ] );
+  // };
+  // const removePost = ( post ) => {
+  //   setPosts( posts.filter( p => p.id !== post.id ) );
+  // };
 
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-  }
+  const [ manyPosts, setPost ] = useState( [
+    { id: 1, title: 'The first', body: 'Description' },
+    { id: 2, title: 'The second', body: 'Description' },
+    { id: 3, title: 'The third', body: 'Description' },
+    { id: 4, title: 'The forth', body: 'Description' },
+  ] );
+  const [ superTitle, setSuperTitle ] = useState( 'List of FANTASTIC POSTS' );
+  const [ somethin, setSomethin ] = useState( 0 );
 
-  const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
-  }
 
   return (
     <div className="App">
-      <PostForm create={createPost}/>
-      <Post remove={removePost} posts={posts} title='Posts List 1'/>
-      <StateClass/>
-      <Counter/>
-      <Toggle />
+      <XCompForm/>
+      <X_component
+        newPosts={ manyPosts }
+        amazingTitle={ superTitle }
+        count={ somethin }
+      />
+
+      {/*<PostForm create={ createPost }/>*/ }
+      {/*{*/ }
+      {/*  posts.length !== 0*/ }
+      {/*    ? <Post remove={ removePost } posts={ posts } title="Posts List 1"/>*/ }
+      {/*    : <div >No post here!!!</div>*/ }
+      {/*      }*/ }
+      {/*      <StateClass/>*/ }
+      {/*      <Counter/>*/ }
+      {/*      <Toggle/>*/ }
     </div>
   );
 }
